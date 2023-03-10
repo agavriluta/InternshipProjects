8beb4ed33d86154a7988c4127e13da3c7f761a6f
agavriluta <agavriluta@griddynamics.com>
2023-03-10 12:13:50 +0200

    first stage task completed

---
+++ EasyProject/src/main/java/org/example/task1_case1/GameStatus.java
@@ -0,0 +1,104 @@
+package org.example.task1_case1;
+
+public class GameStatus {
+    private static char diagonalCheck(char[][] matrix, int col, int row) throws ArrayIndexOutOfBoundsException{
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (i == 0 && j == 0) {
+                    if (matrix[i][j] == matrix[i + 1][j + 1] && matrix[i][j] == matrix[i + 2][j + 2]) {
+                        if (matrix[i][j] == 'X') return 'X';
+                        if (matrix[i][j] == 'O') return 'O';
+                    }
+                } else if (i == col - 1 && j == 0) {
+                    if (matrix[i][j] == matrix[i - 1][j+1] && matrix[i][j] == matrix[i - 2][j + 2]) {
+                        if (matrix[i][j] == 'X') return 'X';
+                        if (matrix[i][j] == 'O') return 'O';
+                    }
+                }
+
+
+            }
+
+        }
+        return '-';
+    }
+
+    private static char horizontalCheck(char[][] matrix, int col, int row) {
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (j == 0) {
+                    if (matrix[i][j] == matrix[i][j + 1] && matrix[i][j] == matrix[i][j + 2]) {
+
+                        if (matrix[i][j] == 'O') {
+                            return 'O';
+                        } else if (matrix[i][j] == 'X') {
+                            return 'X';
+                        }
+                    }
+                }
+            }
+        }
+
+
+        return '-';
+    }
+
+    private static char verticalCheck(char[][] matrix, int col, int row) {
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (i == 0) {
+                    if (matrix[i][j] == matrix[i + 1][j] && matrix[i][j] == matrix[i+2][j]) {
+                        if (matrix[i][j] == 'X') return 'X';
+                        if (matrix[i][j] == 'O') return 'O';
+                    }
+                }
+            }
+
+
+        }
+        return '-';
+    }
+
+    private static boolean fullnessCheck(char[][] matrix, int col, int row) {
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < row; j++) {
+                if (matrix[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static String gameStatus(char[][] matrix, int col, int row) {
+        char diagonal = diagonalCheck(matrix, col, row);
+        char horizontal = horizontalCheck(matrix, col, row);
+        char vertical = verticalCheck(matrix, col, row);
+        boolean full = fullnessCheck(matrix, col, row);
+
+        if (full) {
+            if (diagonal == 'X') return "X wins";
+            else if (diagonal == 'O') return "O wins";
+
+            else if (horizontal == 'X') return "X wins";
+            else if (horizontal == 'O') return "O wins";
+
+            else if (vertical == 'X') return "X wins";
+            else if (vertical == 'O') return "O wins";
+
+            else return "Draw";
+        } else {
+            if (diagonal == 'X') return "X wins";
+            else if (diagonal == 'O') return "O wins";
+
+            else if (horizontal == 'X') return "X wins";
+            else if (horizontal == 'O') return "O wins";
+
+            else if (vertical == 'X') return "X wins";
+            else if (vertical == 'O') return "O wins";
+
+            else return "Game not finished";
+        }
+
+    }
+}
