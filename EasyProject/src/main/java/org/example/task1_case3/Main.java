8c88ca441e1e34155390dd0bd22f3db10f0137a3
agavriluta <agavriluta@griddynamics.com>
2023-04-03 10:40:47 +0300

    created static Matrix object

--- EasyProject/src/main/java/org/example/task1_case3/Main.java
+++ EasyProject/src/main/java/org/example/task1_case3/Main.java
@@ -1,8 +1,11 @@
 package org.example.task1_case3;
 
 public class Main {
+    private static final Matrix matrix;
+    static {
+        matrix = new Matrix();
+    }
     public static void main(String[] args) {
-        Matrix matrix = new Matrix();
         matrix.playTicTacToe();
     }
 }
