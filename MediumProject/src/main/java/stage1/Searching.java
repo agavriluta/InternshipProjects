c09a8ed827f73fec53572c4e6a8a2bf27de8d0d8
agavriluta <agavriluta@griddynamics.com>
2023-04-18 17:55:18 +0300

    added searching solution with stream

--- MediumProject/src/main/java/stage1/Searching.java
+++ MediumProject/src/main/java/stage1/Searching.java
@@ -1,6 +1,7 @@
 package stage1;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Searching {
     private static String[] inputLine(){
@@ -22,6 +23,13 @@ public class Searching {
         }
         if (result.isBlank()) System.out.println("Not Found");
         else System.out.println(result);
+
+//        var var1 = IntStream.range(0, arr.length)
+//                .filter(index -> arr[index].equals(word))
+//                .mapToObj(index -> Integer.valueOf(index).toString())
+//                .findFirst()
+//                .orElse("Not found");
+//        System.out.println(var1);
     }
 
 }
