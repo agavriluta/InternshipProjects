e5c0d3a2e24f16aa589602e7a380e5dd9e65f120
agavriluta <agavriluta@griddynamics.com>
2023-04-24 13:31:10 +0300

    refactored 2nd stage

--- MediumProject/src/main/java/Main.java
+++ MediumProject/src/main/java/Main.java
@@ -2,6 +2,6 @@ import org.example.stage2.Searching;
 
 public class Main {
     public static void main(String[] args) {
-        Searching.searchQueries();
+        Searching.searchEngine();
     }
 }
--- MediumProject/src/main/java/org/example/stage2/Searching.java
+++ MediumProject/src/main/java/org/example/stage2/Searching.java
@@ -1,67 +1,51 @@
 package org.example.stage2;
-
 import java.util.*;
 
 public class Searching {
 
-    private static List<String[]> listOfData(){
+    private static final Scanner scan = new Scanner(System.in);
+    private static List<String> addUsersToGeneralList(){
         System.out.println("Enter the number of people:");
-        Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
         scan.nextLine();
         System.out.println("Enter all people:");
-        List<String[]> data= new ArrayList<>();
-        String[] input = new String[size];
-        for (int i = 0; i < input.length; i++) {
-            input[i] = scan.nextLine();
-        }
-        System.out.println();
+        List<String> userDetailsList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            data.add(input[i].split(" "));
+            userDetailsList.add(scan.nextLine());
         }
-        return data;
+        return userDetailsList;
     }
-    public static void searchQueries(){
-        List<String[]> data = listOfData();
+    public static void searchEngine(){
         System.out.println("Enter the number of search queries:");
-        Scanner scan = new Scanner(System.in);
-        Scanner scanner = new Scanner(System.in);
-        int count = scan.nextInt();
+        int numberOfSearchQueries = scan.nextInt();
         System.out.println();
-        int temp = 0, temp2 = 0;
-        Set<String[]> res = new HashSet<>();
-        for (int i = 0; i < count; i++) {
-            System.out.println("Enter data to search people:");
-            String searchWord = scanner.nextLine();
-            temp2 = 0;
-            res.clear();
-            for (String[] a:data) {
-                temp = 0;
-                for (String s : a) {
-                    if (s.toLowerCase().contains(searchWord.toLowerCase())) ++temp;
-                }
-                if (temp != 0) {
-                    res.add(a);
-                    ++temp2;
-                }
-            }
-            if (temp2 == 0) System.out.println("No matching people found.");
-            else {
-                System.out.println();
-                System.out.println("Found people:");
-                for (String[] a: res) {
-                    for (int j = 0; j < a.length; j++) {
-                        if (j != (a.length - 1)) System.out.print(a[j]+" ");
-                        else System.out.println(a[j]);
-                    }
-
-
-                }
-                System.out.println();
-            }
-
+        Set<String> foundUsers = new HashSet<>();
+        scan.nextLine();
+        for (int i = 0; i < numberOfSearchQueries; i++) {
+            searchUsers(foundUsers);
+            printResult(foundUsers);
         }
+    }
 
+    private static void searchUsers(Set<String> foundUsers) {
+        System.out.println("Enter data to search people:");
+        String searchWord = scan.nextLine();
+        foundUsers.clear();
+        for (String row: addUsersToGeneralList()) {
+            if (row.contains(searchWord)) {
+                foundUsers.add(row);
+            }
+        }
+    }
+
+    private static void printResult(Set<String> res) {
+        if (res.isEmpty()) System.out.println("No matching people found.");
+        else {
+            System.out.println();
+            System.out.println("Found people:");
+            res.forEach(System.out::println);
+            System.out.println();
+        }
     }
 
 }
