1fdb220bf9d905e48478784c75c434e839dedb11
agavriluta <agavriluta@griddynamics.com>
2023-04-25 10:49:42 +0300

    fourth stage edited

--- MediumProject/src/main/java/Main.java
+++ MediumProject/src/main/java/Main.java
@@ -1,4 +1,3 @@
-import stage1.Searching;
 import stage4.Menu;
 import stage4.ReadingFromFile;
 import stage4.UserDetails;
@@ -8,13 +7,12 @@ import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-       startStage4(args);
+       startSearchingStage4(args);
     }
 
-    private static void startStage4(String[] args) {
+    private static void startSearchingStage4(String[] args) {
         if (Objects.equals(args[0], "--data") && args[1] != null) {
             File p = new File(args[1]);
-
             UserDetails.setPeople(ReadingFromFile.readingFromFile(p));
             Menu.selectOption();
         }
