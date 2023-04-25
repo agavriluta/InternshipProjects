7633c1c64d54319dd2200675afe5a2bce5a4e325
agavriluta <agavriluta@griddynamics.com>
2023-04-25 10:55:52 +0300

    fourth stage edited

--- MediumProject/src/main/java/stage4/Menu.java
+++ MediumProject/src/main/java/stage4/Menu.java
@@ -8,35 +8,36 @@ import java.util.function.Consumer;
 public class Menu {
     private static final Scanner scan = new Scanner(System.in);
 
-    private static String printMenu(){
+    private static String printMenu() {
         System.out.println("""
-                    === Menu ===
-                    1. Find a person
-                    2. Print all people
-                    0. Exit""");
+                === Menu ===
+                1. Find a person
+                2. Print all people
+                0. Exit""");
         return scan.nextLine();
 
     }
-    private static final Map<String , Consumer<String>> actionsMap;
+
+    private static final Map<String, Consumer<String>> actionsMap;
+
     static {
-        Consumer<String> find = (search) -> {
-            String input = scan.nextLine();
-            System.out.println("\nEnter a name or email to search all suitable people.");
-            SearchingEngine.searchPerson(input);
-        };
-        Consumer<String> print = (search) -> UserDataOutput.printAllPeople();
-        actionsMap = new HashMap<>();
-        actionsMap.put("1" , find);
-        actionsMap.put("2" , print);
+        actionsMap = Map.of("1",
+                (search) -> {
+                    String input = scan.nextLine();
+                    System.out.println("\nEnter a name or email to search all suitable people.");
+                    SearchingEngine.searchPerson(input);},
+                "2",
+                (search) -> {
+                    UserDataOutput.printAllPeople();});
+
     }
 
-    public static void selectOption(){
+    public static void selectOption() {
         String menuInput = printMenu();
-        while (!menuInput.equals("0")){
-            if (actionsMap.get(menuInput) != null){
+        while (!menuInput.equals("0")) {
+            if (actionsMap.get(menuInput) != null) {
                 actionsMap.get(menuInput).accept(" ");
-            }
-            else {
+            } else {
                 System.out.println("Incorrect option! Try again.");
             }
             menuInput = printMenu();
